0cbdf73db61009970e1a68bb51b090299a4ab15c
Manos Emmanouilidis <esemmano@gmail.com>
2023-02-08 18:56:44 +0200

    publish in debug mode for better traces

--- publish.sh
+++ publish.sh
@@ -1,3 +1,3 @@
-rm -rf publish/
-dotnet publish -o "publish/" -r "win-x64" -c Release --no-self-contained -p:PublishSingleFile=true
-zip -rjm -9 publish/es_to_sql.exe.zip  publish/*
+rm -rf publish/*
+dotnet publish -o "publish/" -r "win-x64" -c Debug --no-self-contained -p:PublishSingleFile=true
+zip -rjm -9 publish/es_to_sql.zip  publish/*
