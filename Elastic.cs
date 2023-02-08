a28f8a7e1b90d4352cd245f48b4688f61bd632e7
Manos Emmanouilidis <esemmano@gmail.com>
2023-02-08 18:57:50 +0200

    use the scroll api to scan indexes
    Elastic queries using (from, size) are not allowed
    when from is over 10K documents
    https://www.elastic.co/guide/en/elasticsearch/reference/5.5/search-request-scroll.html

--- Elastic.cs
+++ Elastic.cs
@@ -55,17 +55,27 @@ public class Elastic
     return new { @bool = new { must = matches } };
   }
 
-  public static async IAsyncEnumerable<Page> EnumerateAllDocumentsInIndex(HttpClient client, string indexName, object query)
+  public static async IAsyncEnumerable<Page> EnumerateAllDocumentsInIndex(HttpClient client, string indexName, int batchSize, object query)
   {
-    int from = 0, size = 1000;
+    int from = 0;
+    string? scrollId = null;
     while (true)
     {
-      var paged_query = new { query = query, from = from, size = size };
-      var queryStr = JsonConvert.SerializeObject(paged_query, Formatting.None);
-      var request = new StringContent(queryStr, Encoding.UTF8, "application/json");
-      var response = await client.PostAsync($"/{indexName}/_search", request);
+      var (url, jsonParams) = PrepareRequest(query, indexName, batchSize, 10, scrollId);
+      var request = new StringContent(jsonParams, Encoding.UTF8, "application/json");
+      var response = await client.PostAsync(url, request);
       var responseBody = await response.Content.ReadAsStringAsync();
       var json = JObject.Parse(responseBody);
+      if (json["error"] != null)
+      {
+        throw new Exception(@$"
+          Received error from elastic while browsing index={indexName}, from={from}, to={from + batchSize - 1}
+          Status code: {response.StatusCode}
+          Headers: {String.Join("; ", response.Headers.Select(h => $"{h.Key}={h.Value}"))}
+          Body: {await response.Content.ReadAsStringAsync()}
+          ");
+      }
+      scrollId = (string?)json["_scroll_id"];
       var docs = ((JArray)json["hits"]["hits"]).Cast<JObject>().ToList();
       var page = new Page
       {
@@ -75,12 +85,25 @@ public class Elastic
         Documents = docs
       };
       yield return page;
-      from += size;
-      if (docs.Count < size)
+      from += batchSize;
+      if (docs.Count < batchSize)
       {
         yield break;
       }
     }
+  }
+
+  private static (string url, string body) PrepareRequest(object query, string indexName, int size, int scrollContextTimeoutSeconds, string? scrollId)
+  {
+    if (scrollId == null)
+    {
+      var queryParams = new { query = query, size = size };
+      var queryStr = JsonConvert.SerializeObject(queryParams, Formatting.None);
+      return ($"/{indexName}/_search?scroll={scrollContextTimeoutSeconds}s", queryStr);
+    }
+    var scrollParams = new { scroll = $"{scrollContextTimeoutSeconds}s", scroll_id = scrollId };
+    var scrollStr = JsonConvert.SerializeObject(scrollParams, Formatting.None);
+    return ("/_search/scroll", scrollStr);
   }
 
   public class Page
@@ -89,7 +112,7 @@ public class Elastic
     public int From { get; set; }
     public int Total { get; set; }
 
-    public string Name { get { return $"{IndexName}:({From}-{Documents.Count - 1})"; } }
+    public string Name { get { return $"{IndexName}:({From}-{Math.Max(From, From + Documents.Count - 1)})"; } }
     public IList<JObject> Documents { get; set; }
   }
 }
